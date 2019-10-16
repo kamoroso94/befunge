774e0dc0e7bbf04ff8479b776ef6ee01b3ff702c
Kyle Amoroso <kamoroso1994@gmail.com>
2019-10-16 01:13:48 -0400

    Fix function signatures

--- befunge_ops.h
+++ befunge_ops.h
@@ -1,36 +1,36 @@
 #ifndef BEFUNGE_OPS_H
 #define BEFUNGE_OPS_H
 
-#include "list.h"
-#include "stack.h"
+#include "befunge.h"
 
-#define BEF_OP(OP) int bef_op_ ## OP(list_t *source_rows, stack_t *stack, size_t x, size_t y)
+#define BEF_OP(OP) int bef_op_ ## OP(bef_state_t *state)
 
-int bef_op_digit(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_add(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_sub(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_mul(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_div(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_mod(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_not(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_gt(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_look_right(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_look_left(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_look_up(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_look_down(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_look_rand_any(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_look_rand_hor(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_look_rand_ver(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_dupe(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_swap(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_discard(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_print_int(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_print_char(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_skip(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_put(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_get(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_scan_int(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_scan_char(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
-int bef_op_exit(list_t *source_rows, stack_t *stack, size_t *x, size_t *y);
+BEF_OP(digit);
+BEF_OP(add);
+BEF_OP(sub);
+BEF_OP(mul);
+BEF_OP(div);
+BEF_OP(mod);
+BEF_OP(not);
+BEF_OP(gt);
+BEF_OP(look_right);
+BEF_OP(look_left);
+BEF_OP(look_up);
+BEF_OP(look_down);
+BEF_OP(look_rand_any);
+BEF_OP(look_rand_hor);
+BEF_OP(look_rand_ver);
+BEF_OP(dupe);
+BEF_OP(swap);
+BEF_OP(discard);
+BEF_OP(print_int);
+BEF_OP(print_char);
+BEF_OP(skip);
+BEF_OP(put);
+BEF_OP(get);
+BEF_OP(scan_int);
+BEF_OP(scan_char);
+// BEF_OP(exit);
+BEF_OP(error);
 
 #endif /* BEFUNGE_OPS_H */
